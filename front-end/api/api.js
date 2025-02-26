d6bf8eae7ad909482df52381bb01b7fa204bb852
Leonardo Rocha Henrique <devleoocha@gmail.com>
2025-02-26 03:26:16 -0300

    Ajuste de link novamente

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://deploy-jornada-full-stack-jp2n.onrender.com/";
+const URL = "https://deploy-jornada-full-stack-jp2n.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
